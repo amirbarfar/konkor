92ef6c43471738e40fe8108aa83f91efba30353f
amirbarfar <amirbarfar43@gmail.com>
2025-12-09 20:57:36 +0330

    fix bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,22 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@/app/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({
+  connectionString : process.env.DATABASE_URL
+})
 
-const connectionString = process.env.DATABASE_URL;
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    adapter : adapter
+  })
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter: new PrismaPg({ connectionString }),
-  });
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
